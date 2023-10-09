9fd7edd6520e35c4b7f96eb508c3980e409ab1bf
MorisGuku <1nara5@mail.ru>
2023-10-09 10:28:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System;
+
+internal class Bag
+{
+    int PizzaPieces = int.TryParse(Console.ReadLine(), out num)
+    int BeerBottles = int.TryParse(Console.ReadLine(), out num)
+    int Capacity = int.TryParse(Console.ReadLine(), out num)
+}
---
+++ bag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_1_hw
+{
+    internal class bag
+    {
+        int PizzaPieces = int.TryParse(Console.ReadLine(), out num)
+        int BeerBottles = int.TryParse(Console.ReadLine(), out num)
+        int Capacity = int.TryParse(Console.ReadLine(), out num)
+    }
+}
