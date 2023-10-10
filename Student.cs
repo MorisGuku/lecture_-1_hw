f92e7bcb86e775ed8defa8b3800f15638afd20c5
MorisGuku <1nara5@mail.ru>
2023-10-10 12:46:38 +0300

    task done

---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using lecture_1_hw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture__1_hw
+{
+    internal class Student
+    {
+        private const int CoefficientEatDrunk = 10;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string CountSatiety(Bag bag)
+        {
+            if (bag.PizzaPieces / (Age / CoefficientEatDrunk) < 1)
+                return "Голоден";
+            else if (bag.PizzaPieces / (Age / CoefficientEatDrunk) == 1)
+                return "В самый раз";
+            else 
+                return "Переел";
+        }
+
+        public string CountDrunk(Bag bag)
+        {
+            if (bag.BeerBottles / (Age/ CoefficientEatDrunk) < 1)
+            {
+                return "Как стеклышко";
+            }
+            else if ((bag.BeerBottles / (Age / CoefficientEatDrunk)) == 1)
+            {
+                return "На веселе";
+            }
+
+            else
+            {
+                return "Домой на автопилоте";
+            }
+        
+        }
+
+    }
+}
--- bag.cs
+++ bag.cs
@@ -1,50 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lecture_1_hw
+﻿namespace lecture_1_hw
 {
-    internal class ClassBag
+    internal class Bag
     {
-       public int PizzaPieces; //кол-во кусков пиццы
-       public int BeerBottles; //кол-во бутылок пива
-       public double Capacity; //максимальный объем рюкзака
-       public double CapacityThisBag; // Объем рюкзака на данный момент
-       public double CapacityPizzaPieces = 0.3; //объем куска пиццы
-       public double CapacityBeerBottles = 0.5; //объем бутылки пива
-       
-      
-       public ClassBag(int PizzaPieces, int BeerBottles, double Capacity) 
-        { 
-            this.PizzaPieces=PizzaPieces;
-            this.BeerBottles=BeerBottles;
-            this.Capacity=Capacity;
-        }
         
-       public double SumCapacity(int SomePieces,double CapacityPieces)
-        {
-            double Sum = SomePieces * CapacityPieces;
-            return Sum;
-        }
+        public int PizzaPieces { get; set; }
+        public int BeerBottles { get; set; }
+        public double Capacity { get; set; }
 
-       public bool AddPizza(int AddPizzaPieces)
+        private const double PizzaCapacity = 0.3;
+        private const double BeerCapacity = 0.5; 
+        public bool AddPizza(int AddPizzaPieces, Bag bag)
         {
-            double CapacityCount = SumCapacity(AddPizzaPieces, this.CapacityPizzaPieces);
-            if (CapacityCount <= this.CapacityThisBag)
+            if (PizzaCapacity * AddPizzaPieces + bag.PizzaPieces * PizzaCapacity + bag.BeerBottles * BeerCapacity <= bag.Capacity)
             {
-                Console.WriteLine($"У тебя уже все забито, сам посмотри! Состояние рюкзака = {this.CapacityThisBag}");
-                return false;
-            }
-            else
-            {
-                Console.WriteLine($"У тебя еще влезет многа! Состояние рюкзака = {this.CapacityThisBag}");
+                bag.PizzaPieces += AddPizzaPieces;
                 return true;
             }
+            return false;
+        }
 
-
-          
+        public bool AddBeer(int AddBearBottles, Bag bag)
+        {
+            if (BeerCapacity * AddBearBottles + bag.PizzaPieces * PizzaCapacity + bag.BeerBottles * BeerCapacity <= bag.Capacity)
+            {
+                bag.BeerBottles += AddBearBottles;
+                return true;
+            }
+            return false;
         }
     }
+
 }
